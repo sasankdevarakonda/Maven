d57b31fca8a8acc0648fdc04865a3ad4464680e5
sasankdevarakonda <149362760+sasankdevarakonda@users.noreply.github.com>
2023-12-15 16:19:59 +0530

    hii

---
+++ src/main/java/transfer.java
@@ -0,0 +1,10 @@
+public class transfer {
+    public static void main(String[]args){
+        classA a =new classA();
+        classb b=new classb();
+        classc c =new classc();
+        a.run();
+        b.run();
+        c.run();
+    }
+}
